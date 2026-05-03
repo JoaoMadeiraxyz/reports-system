6649b9f5531c14dd9182dc089696cbe581796620
JoaoMadeiraxyz <jaodisc@gmail.com>
2026-05-03 01:54:44 -0300

    first

---
+++ src/main/java/xyz/joaomadeira/reportsSystem/ReportsSystem.java
@@ -0,0 +1,16 @@
+package xyz.joaomadeira.reportsSystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ReportsSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
